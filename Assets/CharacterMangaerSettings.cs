e85103d0dc19177204851d75190b4023feee4c15
Nathanael <Goza.wave@gmail.com>
2019-12-22 09:58:13 -0600

    Initial Commit
    Some work on the Player Select Screen and Stage 1

---
+++ Assets/CharacterMangaerSettings.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+
+public class CharacterMangaerSettings : MonoBehaviour
+{
+    //For Player Count (PC)
+    public Sprite[] gallery; //store all your images in here at design time
+    public Image displayImage; //The current image thats visible
+    public int playerCount = 0; //Will control where in the array you are
+    
+    //For Stage Select
+    public TextMeshProUGUI txt;
+    public string[] stages;
+    public int StageNumber = 0;
+
+
+    /*
+    //List<String> charList = { "Jeff", 
+    //                       "Fatty", 
+    //                       "Speedy"};
+    int p1p = 0;
+    int p2p = 0;
+    int p3p = 0;
+    int p4p = 0;
+    */
+
+
+    //Methods for buttons
+    public void BtnNextPC () 
+    {
+    if(playerCount + 1 < gallery.Length)
+        {
+            playerCount++;
+        }
+    }
+
+    public void BtnPrevPC () 
+    {
+        if(playerCount - 1 > 0)
+        {
+            playerCount--;
+        }
+    }
+
+    public void BtnNextStage () 
+    {
+    if(StageNumber + 1 < stages.Length)
+        {
+            StageNumber++;
+        }
+    }
+
+    public void BtnPrevStage () 
+    {
+        if(StageNumber - 1 >= 0)
+        {
+            StageNumber--;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        displayImage.sprite = gallery[playerCount];         //Updates player count icon and stage numver text
+        txt.text = stages[StageNumber];
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rbmain;
+    
+    public float MaxSpeed = 200f;
+    public float MaxTorque = 1f;
+    public string forward = "w";
+    public string left = "a";
+    public string right = "d";
+    public string backward = "s";
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(Input.GetKey(right))
+        {
+            rbmain.AddForce(MaxSpeed * Time.deltaTime, 0, 0);
+        }
+        if(Input.GetKey(left))
+        {
+            rbmain.AddForce(-MaxSpeed * Time.deltaTime, 0, 0);
+        }
+        if(Input.GetKey(forward))
+        {
+            rbmain.AddForce(0, 0, MaxSpeed* Time.deltaTime);
+        }
+        if (Input.GetKey(backward))
+        {
+            rbmain.AddForce(0, 0, -MaxSpeed * Time.deltaTime);
+        }
+    }
+}
