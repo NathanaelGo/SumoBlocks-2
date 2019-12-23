26ce6861eab3429d5f37c484623c39326e54944a
Nathanael <Goza.wave@gmail.com>
2019-12-23 12:47:26 -0600

    Color Update
    Each player now has their own distinct color

--- Assets/StageManager.cs
+++ Assets/StageManager.cs
@@ -12,18 +12,28 @@ public class StageManager : MonoBehaviour
     public int p2Char;
     public int p3Char;
     public int p4Char;
-    public GameObject myPrefab;
-    public GameObject[] chars;
 
+    [Header("Player Models")]
+    public GameObject[] chars;
     public GameObject p1;
     public GameObject p2;
     public GameObject p3;
     public GameObject p4;
 
-    public PlayerMovement p1m;
-    public PlayerMovement p2m;
-    public PlayerMovement p3m;
-    public PlayerMovement p4m;
+
+    public Material p1Mat;
+    public Material p2Mat;
+    public Material p3Mat;
+    public Material p4Mat;
+     
+
+    //Script holders
+    private PlayerMovement p1m;
+    private PlayerMovement p2m;
+    private PlayerMovement p3m;
+    private PlayerMovement p4m;
+
+
 
     void Start()
     {
@@ -42,30 +52,30 @@ public class StageManager : MonoBehaviour
             p1 = Instantiate(chars[p1Char], new Vector3(5, 1, 5), Quaternion.identity);
             p1m = p1.GetComponent<PlayerMovement>();
             p1m.setControl("w","a","d","s");
+            p1.GetComponent<Renderer>().material = p1Mat;
         }
         if(playerCount>=2)
         {
             p2 = Instantiate(chars[p2Char], new Vector3(-5, 1, -5), Quaternion.identity);
             p2m = p2.GetComponent<PlayerMovement>();
             p2m.setControl("up","left","right","down");
+            p2.GetComponent<Renderer>().material = p2Mat;
         }
         if(playerCount>=3)
         {
             p3 = Instantiate(chars[p3Char], new Vector3(5, 1, -5), Quaternion.identity);
             p3m = p3.GetComponent<PlayerMovement>();
             p3m.setControl("f","c","b","v");
+            p3.GetComponent<Renderer>().material = p3Mat;
         }
         if(playerCount>=4)
         {
             p4 = Instantiate(chars[p4Char], new Vector3(-5, 1, 5), Quaternion.identity);
             p4m = p4.GetComponent<PlayerMovement>();
             p4m.setControl("u","h","k","j");
+            p4.GetComponent<Renderer>().material = p4Mat;
         }
 
-        //p2 = Instantiate(chars[0], new Vector3(5, 1, -5), Quaternion.identity);
-
-        //p1m = p1.GetComponent<PlayerMovement>();
-        //p1m.testoo();
     }
 
     // Update is called once per frame
