1aaf902a33878f9c715b4fd4ac05f5b06b33adb2
Nathanael <Goza.wave@gmail.com>
2020-01-08 16:52:17 -0600

    Win Condition Checks [Can now start to determine who wins]
    Debug.Log(The player who has won)

--- Assets/Scripts/GameMode.cs
+++ Assets/Scripts/GameMode.cs
@@ -12,6 +12,8 @@ public class GameMode : MonoBehaviour
     public int[] pStocks;
     
 
+    //        playerCount = PlayerPrefs.GetInt("playerCount", 0);
+
 
     public bool CharDestroyed(int charNum)
     {
@@ -21,13 +23,35 @@ public class GameMode : MonoBehaviour
             if(pStocks[charNum]>0)
                 return true;
             else
+            {
+                if(playersLeftAlive().Length == 1)      //Returns the player number of the last person alive
+                {
+                    Debug.Log(playersLeftAlive()[0]);
+                }
+                else if(playersLeftAlive().Length == 0) //If players die at the exact same time
+                {
+                    Debug.Log("TIE");
+                }
                 return false;
-                
+            }   
             
         }
         return true;
     }
 
+    public int[] playersLeftAlive()       //Return array of player numbers who are alive/left with stocks 
+    {
+        int playerCount = PlayerPrefs.GetInt("playerCount", 0);
+        List<int> playersLeft = new List<int>();
+
+        for(int i = 1; i <= playerCount; i++)
+        {
+            if(pStocks[i]>0)
+                playersLeft.Add(i);
+        }
+        
+        return playersLeft.ToArray();
+    }
 
     // Start is called before the first frame update
     void Start()
